824b5aaf77502b7bc849309f85c2682d4db136d0
Luís Filipe <lufimelo@outlook.com>
2024-08-12 18:15:11 -0300

    Paginação e consultas

--- src/main/java/com/example/springBoot/controllers/ProdutoController.java
+++ src/main/java/com/example/springBoot/controllers/ProdutoController.java
@@ -4,6 +4,8 @@ import com.example.springBoot.model.entities.Produto;
 import com.example.springBoot.model.repositories.ProdutoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -24,6 +26,18 @@ public class ProdutoController {
     @GetMapping
     public Iterable<Produto> obterProdutos(){
         return produtoRepository.findAll();
+    }
+
+    @GetMapping(path = "/nome/{nome}")
+    public Iterable<Produto> obterProdutosPorNome(@PathVariable String nome){
+//        return produtoRepository.findByNomeContaining(nome);
+        return produtoRepository.searchByNameLike(nome);
+    }
+
+    @GetMapping(path = "/pagina/{numeroPagina}")
+    public Iterable<Produto> obterProdutosPorPagina(@PathVariable int numeroPagina){
+        Pageable page = PageRequest.of(numeroPagina, 1);
+        return produtoRepository.findAll(page);
     }
 
     @GetMapping(path = "/{id}")
--- src/main/java/com/example/springBoot/model/repositories/ProdutoRepository.java
+++ src/main/java/com/example/springBoot/model/repositories/ProdutoRepository.java
@@ -1,9 +1,16 @@
 package com.example.springBoot.model.repositories;
 
 import com.example.springBoot.model.entities.Produto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 // DAO != Repository
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends CrudRepository<Produto, Integer>, PagingAndSortingRepository<Produto, Integer> {
 
+    public Iterable<Produto> findByNomeContaining(String nome);
+
+    @Query("SELECT p FROM Produto p WHERE p.nome LIKE %:nome%")
+    public Iterable<Produto> searchByNameLike(@Param("nome") String nome);
 }
